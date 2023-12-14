07efdbe4d4f5b98a5faf19a344196ab8ecfae9d8
avinashkumargh <jonmighty2@gmail.com>
2023-12-14 17:27:02 +0530

    new update for heroku deployment

---
+++ pages/tests.py
@@ -0,0 +1,21 @@
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
+# Create your tests here.
+
+class HomePageTest(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+class AboutPageTest(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
